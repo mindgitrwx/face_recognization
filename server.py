ea78ab23895b9a42ec526216bc774ac276740a4b
Jong Hyun Yeo <37054916+mindgitrwx@users.noreply.github.com>
2018-06-18 23:02:55 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,31 +1,68 @@
-#!/usr/bin/python
+
+from socket import *
 import socket
-import cv2
-import numpy
+import os
+import time
+import sys
 
-def recvall(sock, count):
-    buf = b''
-    while count:
-        newbuf = sock.recv(count)
-        if not newbuf: return None
-        buf += newbuf
-        count -= len(newbuf)
-    return buf
 
-TCP_IP = 'localhost'
-TCP_PORT = 5001
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(True)
-conn, addr = s.accept()
 
-length = recvall(conn,16)
-stringData = recvall(conn, int(length))
-data = numpy.fromstring(stringData, dtype='uint8')
-s.close()
-decimg=cv2.imdecode(data,1)
-#cv2.imshow('SERVER',decimg)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows() 
-cv2.imwrite('result.png',decimg)
+
+def fileName():
+    dte = time.localtime()
+    Year = dte.tm_year
+    Mon = dte.tm_mon
+    Day = dte.tm_mday
+    WDay = dte.tm_wday
+    Hour = dte.tm_hour
+    Min = dte.tm_min
+    Sec = dte.tm_sec
+    imgFileName = str(Year) + '_' + str(Mon) + '_' + str(Day) + '_' + str(Hour) + '_' + str(Min) + '_' + str(
+        Sec) + '.jpg'
+    return imgFileName
+
+
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(("", 5001))
+server_socket.listen(5)
+
+print("TCPServer Waiting for client on port 5000")
+
+while True:
+
+
+    client_socket, address = server_socket.accept()
+
+    print("I got a connection from ", address)
+
+    data = None
+
+
+    while True:
+        img_data = client_socket.recv(1024)
+        data = img_data
+        if img_data:
+            while img_data:
+                print("recving Img...")
+                img_data = client_socket.recv(1024)
+                data += img_data
+            else:
+                break
+
+
+
+    img_fileName = fileName()
+    img_file = open(img_fileName, "wb")
+    print("finish img recv")
+    print(sys.getsizeof(data))
+    img_file.write(data)
+    img_file.close()
+    print("Finish ")
+
+
+
+
+client_socket.close()
+print("SOCKET closed... END")
